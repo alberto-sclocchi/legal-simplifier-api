11707e96bb1c77b000ceff07ced01a3fcebd0ad6
Alberto Sclocchi <scloks75tv@gmail.com>
2025-07-14 22:30:40 +0200

    add open ai api to retrieve simplified text version

--- routes/simplifier.routes.js
+++ routes/simplifier.routes.js
@@ -5,11 +5,13 @@ const fs = require("fs");
 const pdfParser = require("pdf-parse");
 const path = require("path");
 const PDFDocument = require('pdfkit');
+const OpenAI = require("openai");
 
 
 
 
 const upload = multer({ dest: "uploads/" });
+const client = new OpenAI({apiKey: process.env.OPEN_AI_API_KEY});
 
 router.post("/", upload.single("pdfFile"), async (req, res, next) => {
   const fileBuffer = fs.readFileSync(req.file.path);
@@ -26,12 +28,30 @@ router.post("/", upload.single("pdfFile"), async (req, res, next) => {
   try{
     const data = await pdfParser(fileBuffer);
 
+    const response = await client.responses.create({
+      model: "gpt-4.1",
+      input: [
+        {
+          role: "user",
+          content: `Please simplify the following legal text:\n\n${data.text}`
+        },
+        {
+          role: "developer",
+          content: "You are a legal expert specialized in simplifying complex legal documents. First, verify if the provided text is a legal document. This includes—but is not limited to—contracts, terms of service, privacy policies, ADA compliance statements, waivers, liability disclaimers, legal notices, or regulatory agreements. If it is not, reply clearly: 'error'. Divide it in sections and provide a clear, concise summary of each section. Use simple language and avoid legal terms. Add emojis to make it more engaging. The output should be a structured summary with headings for each section, and the text should be easy to read and understand. Do not ask for any clarifications or additional information."
+        }
+      ]
+    });
+
     const doc = new PDFDocument();
     doc.pipe(fs.createWriteStream(outputFilePath));
-    doc.text(data.text);
+    doc.text(response.output_text);
     doc.end();
 
-    res.json({success: true, filename: req.file.filename, text: data.text});
+    let outputFileName = req.file.originalname;
+    
+    if (outputFileName.lastIndexOf(".") !== -1) outputFileName = outputFileName.substring(0, outputFileName.lastIndexOf("."));
+
+    res.json({success: true, filename: req.file.filename, fileOriginalName: outputFileName, text: response.output_text});
   } catch (err){
     res.json({success: false, result: err})
   } finally{
